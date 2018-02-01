0404b06862b5f555d4a34667bc5305a7f2937390
Alex <busarovalex@yandex.ru>
2018-02-02 00:09:47 +0300

    remove vocabulry from binary

--- src/main.rs
+++ src/main.rs
@@ -71,11 +71,6 @@ fn main() {
     let token = env::var("TELEGRAM_BOT_TOKEN").unwrap();
     let api = Api::configure(token).build(core.handle()).unwrap();
 
-    let vocabulary = include_str!("../10kwords.txt");
-    let words: Vec<_> = vocabulary.lines()
-        .collect();
-
-
     // Fetch new updates via long poll method
     let future = api.stream().for_each(|update| {
 
