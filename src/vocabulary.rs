2651d1b93696b8ffd84052f98a0617d6ac42a8b8
Alex <busarovalex@yandex.ru>
2018-02-02 17:50:20 +0300

    statistics

--- src/vocabulary.rs
+++ src/vocabulary.rs
@@ -1,6 +1,13 @@
 pub struct Vocabulary<'r> {
-    by_length: Vec<Vec<&'r str>>
+    by_length: Vec<Vec<&'r str>>,
+    statistics: Statistics
 }
+
+pub struct Statistics {
+    pub length_distribution: LengthDistribution
+}
+
+pub struct LengthDistribution(Vec<usize>);
 
 impl<'r> Vocabulary<'r> {
     pub fn new(words: &'r [&'r str]) -> Vocabulary<'r> {
@@ -13,12 +20,22 @@ impl<'r> Vocabulary<'r> {
             }
             by_length[word.len()].push(*word);
         }
+        let length_distribution = by_length.iter().map(Vec::len).collect();
         Vocabulary {
-            by_length
+            by_length,
+            statistics: Statistics {
+                length_distribution: LengthDistribution(length_distribution)
+            }
         }
     }
 
     pub fn with_length(&'r self, word_len: usize) -> Result<&'r [&'r str], String> {
         self.by_length.get(word_len).map(|v| v.as_slice()).ok_or(format!("No words with length {}", word_len))
+    }
+}
+
+impl LengthDistribution {
+    pub fn get(&self, word_length: usize) -> Option<usize> {
+        self.0.get(word_length).cloned()
     }
 }
